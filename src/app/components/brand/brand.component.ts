35539ce063f8d0b35f43b03c78c28a032bf87111
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2021-03-11 19:13:48 +0300

    markalar listelendi

--- src/app/components/brand/brand.component.ts
+++ src/app/components/brand/brand.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Brand } from 'src/app/models/brand';
 import { BrandService } from 'src/app/services/brand.service';
+import {brandResponseModel} from 'src/app/models/brandResponseModel'
 
 @Component({
   selector: 'app-brand',
