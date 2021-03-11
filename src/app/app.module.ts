7aff1bc2f47a7ab15a3965cdb8618b427cdc3488
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2021-03-11 17:35:21 +0300

    11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarComponent } from './car/car.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { ColorComponent } from './components/color/color.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { RentalComponent } from './components/rental/rental.component';
+import { CarImageComponent } from './components/car-image/car-image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent,
+    BrandComponent,
+    ColorComponent,
+    CustomerComponent,
+    RentalComponent,
+    CarImageComponent
   ],
   imports: [
     BrowserModule,
